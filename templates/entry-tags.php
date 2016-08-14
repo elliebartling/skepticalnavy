30efaafb38571d6bf068f6bee23dfa4316a86973
Ellen Bartling <ellenbartling@gmail.com>
2016-08-14 12:40:37 -0500

    Constrain figures to 100% and limit number of tags

--- templates/entry-tags.php
+++ templates/entry-tags.php
@@ -1,2 +1,13 @@
-<div class="tags"><?= get_the_tag_list('<span>', '&nbsp;', '</span>'); ?>
+<div class="tags">
+  <?php
+    $posttags = get_the_tags();
+    $count = 0;
+    if ($posttags) {
+    	foreach($posttags as $tag) {
+    		$count++;
+    		echo '<span><a href="'.get_tag_link($tag->term_id).'">'.$tag->name.'</a> </span>';
+    		if( $count > 3 ) break;
+    	}
+    }
+    ?>
 </div>
